af906295a13c29d5b99637b9080e642c2d67f7b5
Jon <jon.l@agencywithin.com>
2019-03-20 14:05:44 -0400

    factions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const factions = require('./data/factions/factions.json');
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        Hello
-        </header>
-      </div>
-    );
-  }
+    buildOptions() {
+        const arr = [];
+
+        for (let f of factions) {
+            arr.push(
+                <option key={f.xws} value="{f.xws}">
+                    {f.name}
+                </option>,
+            );
+        }
+
+        return arr;
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    Hello
+                    <select>{this.buildOptions()}</select>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
